10f9fd169d02875de5bb9bd89a22e45cff2dac10
CrescentRose <CrescentRose@ninja5.d22.lan>
2015-09-30 10:40:15 -0500

    This is my code

---
+++ NetBeansProjects/Reiler_7_primes/src/reiler_7_primes/Reiler_7_primes.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reiler_7_primes;
+
+/**
+ *
+ * @author CrescentRose
+ */
+public class Reiler_7_primes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if(i % 2 != 0) {
+              if(i % 3 != 0) {
+                if(i % 5 != 0) {
+                     System.out.println(i + " is not even");   
+                }  
+              }
+                           
+            
+            
+             
+            
+            }
+          
+        }
+        
+    }
+    
+}
